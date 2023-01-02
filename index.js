f0e12fabdfcebc79be8f779bfebc0f2724e3c023
Samiulahmedanik <84468942+SAANIK45@users.noreply.github.com>
2023-01-02 20:58:19 +0600

    Get user by email

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-const bodyParser = require('body-parser');
+const bodyParser = require("body-parser");
 const { MongoClient } = require("mongodb");
 const ObjectId = require("mongodb").ObjectId;
 require("dotenv").config();
@@ -12,7 +12,6 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 // app.use(express.json());
 app.use(bodyParser.json());
-
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.iy3km.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, {
@@ -27,8 +26,31 @@ const run = async () => {
     const database = client.db("job-hunt");
     const usersCollection = database.collection("users");
 
-   
+    app.get("/users", async (req, res) => {
+      const cursor = usersCollection.find({});
+      const users = await cursor.toArray();
+      res.send({ status: true, data: users });
+    });
 
+    app.post("/user", async (req, res) => {
+      const user = req.body;
+      const result = await usersCollection.insertOne(user);
+      res.send({ status: true, data: result });
+    });
+
+    app.get("/user/:email", async (req, res) => {
+      const email = req.params.email;
+      console.log("hit",email);
+
+      const result = await usersCollection.findOne({ email });
+      console.log(result)
+      if (result?.email) {
+        return res.send({ status: true, data: result });
+      
+      }
+
+      res.send({ status: false });
+    });
   } finally {
   }
 };
@@ -38,7 +60,6 @@ run().catch((err) => console.log(err));
 app.get("/", (req, res) => {
   res.send("Job-Hunt-Server-Running");
 });
-
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
